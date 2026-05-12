f9a553d9fd17398b94a49a1a985cd0fc9f961ea4
Tohid Mulla <tohidmulla444@gmail.com>
2026-05-12 23:48:24 +0530

    understanded the flow of ledger and implenmeted the Transaction Model

---
+++ src/models/transaction.model.js
@@ -0,0 +1,28 @@
+const mongoose=require("mongoose")
+
+
+
+
+
+const transactionSchema=new mongoose.Schema({
+
+    fromAcccount:{
+        
+    },
+    toAccount:{
+
+    },
+    amount:{
+
+    },
+
+     status:{
+
+     },
+     idempostencyKey:{
+
+     },
+     
+    },
+    {timestamps:true}
+)
